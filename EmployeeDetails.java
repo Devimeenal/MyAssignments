eda654bd999500f7ed47cab0bb715c77bd73258a
Devimeenal <devimeenal.pk@gmail.com>
2025-12-09 12:36:41 +0530

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		short age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		 boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		int pinCode = 56001;
+		byte luckyNumber = 7;		
+		
+		System.out.println( "Priniting all variable values:\n\n" +
+		"Name : "+name+"\n"+
+		 "Age:"+age+"\n"+
+		 "salary:"+salary+"\n"+
+		 "gender :"+ gender+"\n"+
+		  "acceptTerms :"+acceptTerms+"\n"+
+		  "rating :"+rating+ "\n"+
+		  "mobileNumber: "+mobileNumber+"\n"+
+		  "poicode:"+pinCode+"\n"+
+		  "luckyNumber:"+luckyNumber+"\n");				
+
+	}
+
+}
