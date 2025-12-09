3946efcc378df67a37bddc4cde0505673f4f7275
Devimeenal <devimeenal.pk@gmail.com>
2025-12-09 16:52:47 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+package week1.day2;
+
+public class Fibonacci {
+
+	public static void fib(int N){
+		 int n1 = 0, n2 = 1;
+	        for (int i = 0; i < N; i++)
+	        {
+	            System.out.print(n1 + " ");
+	            int n3 = n2 + n1;
+	            n1 = n2;
+	            n2 = n3;	
+	        }
+	}
+		public static void main(String[] args) {
+		int N = 10;
+        fib(N);
+	
+	}
+        
+        
+		
+		
+		
+	}
+
+	
+
